ddb2485c7653125e61c5abfa73c63356d47a20eb
Ian Bailey <IanKulin@kulin.com.au>
2022-11-12 15:40:23 +0800

    Remove unused response code

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -9,10 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
 
-    struct Response: Codable {
-        var users: [User]
-    }
-
     @State private var users = [User]()
 
     var body: some View {
@@ -45,7 +41,7 @@ struct ContentView: View {
         do {
             let decoder = JSONDecoder()
             decoder.dateDecodingStrategy = .iso8601
-            
+
             let (data, _) = try await URLSession.shared.data(from: url)
             do {
                 let decodedUsers = try decoder.decode([User].self, from: data)
